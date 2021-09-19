9b002d1de12f4045f892c52e70bf69fca762c014
Jeiffer Garcia <jeiffergarcia@gmail.com>
2021-09-19 13:56:16 -0500

    maquetacion desktop

---
+++ src/container/Home.js
@@ -0,0 +1,12 @@
+
+
+const Home = () => {
+  const view = `
+    <div>
+     <h1>Hola Home Angie</h1>
+    </div>
+  `;
+  return view;
+};
+
+export default Home;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import Home from "./container/Home"
+
+import "./styles/styles.css"
+
+const app = document.getElementById("app")
+
+app.innerHTML = Home();
